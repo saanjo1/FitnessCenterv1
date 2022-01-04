3f308800d42ab0036d4ba416e6dc2108707d2f65
Sanjo <sanjin.golos@edu.fit.ba>
2022-01-04 22:59:30 +0100

    CRUD - Sponsors

---
+++ FitnessCenter.Web/ViewModels/SponsorsIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿using FitnessCenter.Data.Entities;
+
+namespace FitnessCenter.Web.ViewModels
+{
+    public class SponsorsIndexViewModel
+    {
+        public List<Sponsor> Sponsors { get; set; }
+    }
+}
