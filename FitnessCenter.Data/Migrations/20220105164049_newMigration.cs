f9e7ca80721863f249dc8d505aa1b0d30895c56f
DESKTOP-G9VM294\Muhamed <theatoriss@gmail.com>
2022-01-05 18:14:35 +0100

    Edit

---
+++ FitnessCenter.Data/Migrations/20220105164049_newMigration.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitnessCenter.Data.Migrations
+{
+    public partial class newMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PhotoId",
+                table: "Contacts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_PhotoId",
+                table: "Contacts",
+                column: "PhotoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contacts_Photo_PhotoId",
+                table: "Contacts",
+                column: "PhotoId",
+                principalTable: "Photo",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contacts_Photo_PhotoId",
+                table: "Contacts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Contacts_PhotoId",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "PhotoId",
+                table: "Contacts");
+        }
+    }
+}
