e8c257ef2ef1283810859ec4d147068c60efdfe6
Sanjo <sanjin.golos@edu.fit.ba>
2022-01-07 15:19:47 +0100

    Manage contacts - adding a new contact

---
+++ FitnessCenter.Web/MappingProfiles/Contact.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using FitnessCenter.Data.Entities;
+using FitnessCenter.Web.ViewModels;
+
+namespace FitnessCenter.Web.MappingProfiles
+{
+    public class ContactProfile : Profile
+    {
+        public ContactProfile()
+        {
+            CreateMap<Contact, ContactsManageViewModel>();
+            CreateMap<ContactsManageViewModel, Contact>().ForMember(x => x.Photo, obj => obj.Ignore());
+        }
+    }
+}
---
+++ FitnessCenter.Web/ViewModels/ContactsManageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using FitnessCenter.Data.Entities;
+using FitnessCenter.Web.Resources;
+using System.ComponentModel.DataAnnotations;
+
+namespace FitnessCenter.Web.ViewModels
+{
+    public class ContactsManageViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = nameof(Name), ResourceType = typeof(Translations))]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = nameof(Value), ResourceType = typeof(Translations))]
+        public string Value { get; set; }
+
+        [Required]
+        [Display(Name = nameof(Translations.User), ResourceType = typeof(Translations))]
+        public int UserId { get; set; }
+
+        [Required]
+        [Display(Name = nameof(Translations.Photo), ResourceType = typeof(Translations))]
+        public IFormFile Photo { get; set; }
+    }
+}
